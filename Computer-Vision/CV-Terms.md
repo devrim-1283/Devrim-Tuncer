d0f9eb7f7dd45bd6e9aa5564da53fe9ae06476ab
Devrim Tunçer <devrimtuncer.ai@gmail.com>
2025-02-21 17:11:19 +0300

    Computer Vision Note added and Internship-Dictonary added

---
+++ Computer-Vision/CV-Terms.md
@@ -0,0 +1,19 @@
+#Computer Vision Terms:
+1. Image Processing
+	- Resim İşleme
+	- Yapay Zeka yok
+2. Image Classification
+	- Resim Sınıflandırma
+	- ANN (Yapay Sinir Ağları)
+	- Yapay Zeka var
+	- Nesneyi sınıfa atıyoruz.
+3. Object Detection
+	- Konum tespit etme x,y
+4. Image Segmentation
+	- Pixel tabanlı tespit etme
+	- Görüntünün tamamına etiket atıyor
+5. Keypoint Detection
+	- Belirlenene özel keypointleri tespit eder
+6. Face Recognition Systems
+	- Kimlik çıkarma
+- Image processing dışında her uygulamada yapay zeka modelleri kullanacağız.
---
+++ Computer-Vision/Trackbar.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+
+
+def nothing(x):
+    pass
+
+
+img = np.zeros((512,512,3),dtype="uint8")
+cv2.namedWindow("Image")
+
+
+cv2.createTrackbar("Switch","Image",0,1,nothing)
+cv2.createTrackbar("R","Image",0,255,nothing)
+cv2.createTrackbar("G","Image",0,255,nothing)
+cv2.createTrackbar("B","Image",0,255,nothing)
+
+
+
+
+while True:
+
+    cv2.imshow("Image",img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+
+    r = cv2.getTrackbarPos("R","Image")
+    g = cv2.getTrackbarPos("G","Image")
+    b = cv2.getTrackbarPos("B","Image")
+    if cv2.getTrackbarPos("Switch","Image")==1:
+        img[:] = [b,g,r]
+    else:
+        img[:] = (0,0,0)
+
+
+cv2.destroyAllWindows()
